c33539326f5e8eabf9d92a085bd726475c2292bf
Adel Kamal <adelkamal911@gmail.com>
2023-11-02 19:27:55 +0200

    remove comments

--- SystemDesign/src/main/java/com/example/SystemDesign/Config/ApplicationConfig.java
+++ SystemDesign/src/main/java/com/example/SystemDesign/Config/ApplicationConfig.java
@@ -1,7 +1,6 @@
 package com.example.SystemDesign.Config;
 
 import com.example.SystemDesign.Repository.UserRepository;
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -14,7 +13,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
-//@RequiredArgsConstructor
 public class ApplicationConfig {
 
     private final UserRepository userRepository;
